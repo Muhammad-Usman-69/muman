95d87551015c4c718d619b2bb4013eaf8cac146b
Muhammad Usman <usmansaleem4446996@gmail.com>
2023-11-15 10:10:36 +0500

    Improvements and Sale section added

--- script.js
+++ script.js
@@ -19,3 +19,45 @@ leftButton.addEventListener("click", () => {
     currentPosition -= scrollAmount;
     bannerContainer.scrollLeft = currentPosition;
 });
+
+/* window.addEventListener("load", () => {
+    setInterval(() => {
+        currentPosition += scrollAmount;
+        bannerContainer.scrollLeft = currentPosition;
+        if (currentPosition == (scrollAmount * bannerImg.length)) {
+            bannerContainer.scrollLeft = 0;
+            currentPosition = 0;
+        }
+    }, 3000);
+}) */
+
+function updateCountdown() {
+    let curDate = Date.now();
+    const desDate = new Date("2030/1/1");
+    let milliDesDate = desDate.getTime();
+    let newDate = milliDesDate - curDate;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(newDate / day);
+    const hours = Math.floor((newDate % day) / hour);
+    const minutes = Math.floor((newDate % hour) / minute);
+    const seconds = Math.floor((newDate % minute) / second);
+
+    const paddedDays = days < 10 ? `0${days}` : days;
+    const paddedHours = hours < 10 ? `0${hours}` : hours;
+    const paddedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const paddedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    document.querySelector(".hours").innerHTML = paddedHours;
+    document.querySelector(".minutes").innerHTML = paddedMinutes;
+    document.querySelector(".seconds").innerHTML = paddedSeconds;
+}
+
+setInterval(updateCountdown, 1000);
+
+
+
