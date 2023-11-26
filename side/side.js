da4a6ec6b6d06da8fbaec1e1e2875d3258c5eba2
Muhammad Usman <usmansaleem4446996@gmail.com>
2023-11-26 22:08:19 +0500

    cart basic functionability created

--- side/side.js
+++ side/side.js
@@ -36,16 +36,72 @@ quantities.forEach((quantity, index) => {
         }
     })
 });
-    /* pluses.forEach((plus) => {
-    })
-    minuses.forEach((minus) => {
-        minus.addEventListener("click", () => {
-            if (quantity.innerHTML <= 1) {
-                minus.disabled = true;
-            } else {
-                minus.disabled = false;
-                quantity.innerHTML--;
+
+let items = document.querySelectorAll(".item");
+document.querySelector("title").innerHTML =
+    items.length + " items found in Cart";
+
+let prices = document.querySelectorAll(".price");
+let selectBtn = document.querySelectorAll(".select");
+let selectAll = document.querySelector(".selectAll");
+let totalItem = document.querySelectorAll(".totalItem");
+let totalPrice = document.querySelector(".totalPrice");
+let finalPrice = document.querySelector(".finalPrice");
+
+selectBtn.forEach((btn, index) => {
+    let price = Number(prices[index].innerHTML);
+    
+    selectAll.addEventListener("input", () => {
+        if (selectAll.checked == true) {
+            if (btn.checked == false) {
+                btn.checked = true;
+                totalItem.forEach((total) => {
+                    total.innerHTML++;
+                })
+                if (totalPrice.innerHTML == 0) {
+                    totalPrice.innerHTML = (price).toFixed(2);
+                    finalPrice.innerHTML = (4 + Number(totalPrice.innerHTML)).toFixed(2);
+                } else {
+                    totalPrice.innerHTML = (Number(totalPrice.innerHTML) + price).toFixed(2);
+                    finalPrice.innerHTML = (4 + Number(totalPrice.innerHTML)).toFixed(2);
+                }
             }
-        });
-    }) */
-/* }) */
+        } else {
+            btn.checked = false;
+            totalItem.forEach((total) => {
+                total.innerHTML--;
+            })
+            totalPrice.innerHTML = (Number(totalPrice.innerHTML) - price).toFixed(2);
+            if (totalPrice.innerHTML == 0) {
+                finalPrice.innerHTML = 0;
+            } else {
+                finalPrice.innerHTML = (Number(totalPrice.innerHTML) + 4).toFixed(2);
+            }
+        }
+    })
+
+    btn.addEventListener("input", () => {
+        if (btn.checked == true) {
+            totalItem.forEach((total) => {
+                total.innerHTML++;
+            })
+            if (totalPrice.innerHTML == 0) {
+                totalPrice.innerHTML = (price).toFixed(2);
+                finalPrice.innerHTML = (4 + Number(totalPrice.innerHTML)).toFixed(2);
+            } else {
+                totalPrice.innerHTML = (Number(totalPrice.innerHTML) + price).toFixed(2);
+                finalPrice.innerHTML = (4 + Number(totalPrice.innerHTML)).toFixed(2);
+            }
+        } else {
+            totalItem.forEach((total) => {
+                total.innerHTML--;
+            })
+            totalPrice.innerHTML = (Number(totalPrice.innerHTML) - price).toFixed(2);
+            if (totalPrice.innerHTML == 0) {
+                finalPrice.innerHTML = 0;
+            } else {
+                finalPrice.innerHTML = (Number(totalPrice.innerHTML) + 4).toFixed(2);
+            }
+        }
+    })
+})
