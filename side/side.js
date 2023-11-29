394c180f71f58b6686c692f43d14529986b7f218
Muhammad Usman <usmansaleem4446996@gmail.com>
2023-11-29 10:29:16 +0500

    Cart functionablity completed and improved

--- side/side.js
+++ side/side.js
@@ -17,91 +17,83 @@ inputs.forEach((input, index) => {
 let quantities = document.querySelectorAll(".quantity-number");
 let plusAll = document.querySelectorAll(".plus");
 let minusAll = document.querySelectorAll(".minus");
-
-quantities.forEach((quantity, index) => {
-    let plus = plusAll[index];
-    let minus = minusAll[index];
-    plus.addEventListener("click", () => {
-        quantity.innerHTML++;
-        if (quantity.innerHTML > 1) {
-            minus.disabled = false;
-        }
-    })
-    minus.addEventListener("click", () => {
-        if (quantity.innerHTML <= 1) {
-            minus.disabled = true;
-        } else {
-            minus.disabled = false;
-            quantity.innerHTML--;
-        }
-    })
-});
-
 let items = document.querySelectorAll(".item");
-document.querySelector("title").innerHTML =
-    items.length + " items found in Cart";
-
 let prices = document.querySelectorAll(".price");
 let selectBtn = document.querySelectorAll(".select");
 let selectAll = document.querySelector(".selectAll");
 let totalItem = document.querySelectorAll(".totalItem");
-let totalPrice = document.querySelector(".totalPrice");
+let totalPriceBf = document.querySelector(".totalPrice");
 let finalPrice = document.querySelector(".finalPrice");
+let deliveryCharge = document.querySelector(".delivery");
+
+document.querySelector("title").innerHTML =
+    items.length + " items found in Cart";
 
 selectBtn.forEach((btn, index) => {
-    let price = Number(prices[index].innerHTML);
-    
-    selectAll.addEventListener("input", () => {
-        if (selectAll.checked == true) {
-            if (btn.checked == false) {
-                btn.checked = true;
-                totalItem.forEach((total) => {
-                    total.innerHTML++;
-                })
-                if (totalPrice.innerHTML == 0) {
-                    totalPrice.innerHTML = (price).toFixed(2);
-                    finalPrice.innerHTML = (4 + Number(totalPrice.innerHTML)).toFixed(2);
-                } else {
-                    totalPrice.innerHTML = (Number(totalPrice.innerHTML) + price).toFixed(2);
-                    finalPrice.innerHTML = (4 + Number(totalPrice.innerHTML)).toFixed(2);
-                }
-            }
-        } else {
-            btn.checked = false;
-            totalItem.forEach((total) => {
-                total.innerHTML--;
-            })
-            totalPrice.innerHTML = (Number(totalPrice.innerHTML) - price).toFixed(2);
-            if (totalPrice.innerHTML == 0) {
-                finalPrice.innerHTML = 0;
-            } else {
-                finalPrice.innerHTML = (Number(totalPrice.innerHTML) + 4).toFixed(2);
-            }
+    let quantity = quantities[index];
+    let minus = minusAll[index];
+    let plus = plusAll[index];
+
+    plus.addEventListener("click", () => {
+        quantity.innerHTML++;
+        updateTotal();
+    });
+
+    minus.addEventListener("click", () => {
+        if (quantity.innerHTML > 1) {
+            quantity.innerHTML--;
+            updateTotal();
         }
-    })
+    });
 
     btn.addEventListener("input", () => {
-        if (btn.checked == true) {
-            totalItem.forEach((total) => {
-                total.innerHTML++;
-            })
-            if (totalPrice.innerHTML == 0) {
-                totalPrice.innerHTML = (price).toFixed(2);
-                finalPrice.innerHTML = (4 + Number(totalPrice.innerHTML)).toFixed(2);
-            } else {
-                totalPrice.innerHTML = (Number(totalPrice.innerHTML) + price).toFixed(2);
-                finalPrice.innerHTML = (4 + Number(totalPrice.innerHTML)).toFixed(2);
+        updateTotal();
+    });
+});
+
+function updateTotal() {
+    let totalQuantity = 0;
+    let totalPrice = 0;
+    let oneChecked = false;
+
+    selectBtn.forEach((btn, index) => {
+        let quantity = quantities[index];
+        let price = Number(prices[index].innerHTML);
+
+        if (btn.checked) {
+            totalQuantity += Number(quantity.innerHTML);
+            totalPrice += price * Number(quantity.innerHTML);
+            oneChecked = true;
+        }
+
+        setTimeout(() => {
+            if (btn.checked == false) {
+                selectAll.checked = false;
             }
+        }, 1);
+    });
+
+    totalItem.forEach((total) => {
+        total.innerHTML = totalQuantity;
+    });
+
+    totalPriceBf.innerHTML = (totalPrice).toFixed(2);
+    
+    if (oneChecked) {
+        finalPrice.innerHTML = (totalPrice + Number(deliveryCharge.innerHTML)).toFixed(2);
+    } else {
+        finalPrice.innerHTML = "0.00";
+    }
+}
+
+selectAll.addEventListener("input", () => {
+    selectBtn.forEach((btn) => {
+        if (selectAll.checked == true) {
+            btn.checked = true;
+            updateTotal();
         } else {
-            totalItem.forEach((total) => {
-                total.innerHTML--;
-            })
-            totalPrice.innerHTML = (Number(totalPrice.innerHTML) - price).toFixed(2);
-            if (totalPrice.innerHTML == 0) {
-                finalPrice.innerHTML = 0;
-            } else {
-                finalPrice.innerHTML = (Number(totalPrice.innerHTML) + 4).toFixed(2);
-            }
+            btn.checked = false;
+            updateTotal();
         }
     })
 })
